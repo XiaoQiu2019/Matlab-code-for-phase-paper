e4b7b486e457410b690dacdc592149972b9a11f9
XiaoQiu2019 <45832556+XiaoQiu2019@users.noreply.github.com>
2019-02-28 12:10:07 -0500

    Update Kuramoto_synch_measurement.m

--- Kuramoto_synch_measurement.m
+++ Kuramoto_synch_measurement.m
@@ -1,4 +1,4 @@
 Ktwocell=kuramoto_within_twocell_droplet(cell2);%kuramoto number within two cells droplets
 Konecellk=kuramoto_within_onecell(cell1,RhB);%kuramoto number within single cell droplets
 Kbetweentwocell=kuramoto_between_twocell(cell2);%kuramoto number between two cells droplets
-Kbetweenonecell=droplet_between_onecell(cell1,RhB);%kuramoto number between single cell droplets
+Kbetweenonecell=kuramoto_between_onecell(cell1,RhB);%kuramoto number between single cell droplets

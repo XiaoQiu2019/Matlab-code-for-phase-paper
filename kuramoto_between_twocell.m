8afdbe4d77a6f9a8c80ce1add4f68eb45f839fcf
XiaoQiu2019 <45832556+XiaoQiu2019@users.noreply.github.com>
2019-04-03 12:35:25 -0400

    Update kuramoto_between_twocell.m

--- kuramoto_between_twocell.m
+++ kuramoto_between_twocell.m
@@ -1,4 +1,4 @@
-function Kbetweentwocell=kuramoto_between_twocell(g)
+function K=kuramoto_between_twocell(g)
 %calculate K between two cell droplets
 %detrend data
 res=dtrndanl2(g);
@@ -6,13 +6,11 @@ res=dtrndanl2(g);
 for i=1:size(res,1)
 [phi(i,:),phi_J2(i,:),phi_adj(i,:),im_analytic_signal_x(i,:),re_analytic_signal_x(i,:)]= hilbert_phase(res(i,:));
 end
-
-for j=1:2:size(phi,1)
-twocellb=phi(j:j+1,:);
-s=mean(twocellb);
-[m, n] = size (s);
-mean_phi=mean(mean(exp(complex(0,1)*s)));
-F(j,:)=sum(abs(sum(exp(complex(0,1)*s))-mean_phi))/(m*n);
+%get average phi in eanch droplet
+p=zeros(568,563);
+for j=1:568
+    s=mean([phi((2*j-1),:);phi((2*j),:)]);
+    p(j,:)=s;
 end
-Kbetweentwocell=sum(F)/568;
+K=kuramoto2(p);
 end

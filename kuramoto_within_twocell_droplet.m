fed9df4cff1340e97a72e1cd3cd0f5e4269fb61a
XiaoQiu2019 <45832556+XiaoQiu2019@users.noreply.github.com>
2019-04-08 10:38:38 -0400

    Update kuramoto_within_twocell_droplet.m

--- kuramoto_within_twocell_droplet.m
+++ kuramoto_within_twocell_droplet.m
@@ -1,19 +1,18 @@
-function Ktwocell=kuramoto_within_twocell_droplet(g)
+function [G2,K2]=kuramoto_within_twocell_droplet(g)
 %%two cell droplet data 
 %detrend data
 res=dtrndanl2(g);
 %run hilbert phase code
 for i=1:size(res,1)
-[phi(i,:),phi_J2(i,:),phi_adj(i,:),im_analytic_signal_x(i,:),re_analytic_signal_x(i,:)]= hilbert_phase(res(i,:));
+    [phi(i,:),phi_J2(i,:),phi_adj(i,:),im_analytic_signal_x(i,:),re_analytic_signal_x(i,:)]= hilbert_phase(res(i,:));
 end
 %obtain K for two cells within one droplet
-m=size(phi,2);
-for j=1:2:size(phi,1)
-twocelld=phi(j:j+1,:);
-n=size(twocelld,1);
-mean_phi=mean(mean(exp(complex(0,1)*twocelld)));
-G(j,:)=sum(abs(sum(exp(complex(0,1)*twocelld))-mean_phi))/(m*n);
+G2=zeros(568,1);
+for j=1:568
+    twocell=[phi((2*j-1),:);phi(2*j,:)];
+    k=kuramoto2(twocell);
+    G2(j,1)=k;
 end
-Ktwocell=sum(G)/568;
+K2=sum(G2)/568;
 end
 
